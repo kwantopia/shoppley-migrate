787633e4e60ae1f4bdf4a2a14b0c5f84b6bc4abd
dawei <dawei@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-02-21 17:38:13 +0000

    SMS functionality is sort of working

--- shoppley.com/shoppley/apps/offer/management/commands/check_sms.py
+++ shoppley.com/shoppley/apps/offer/management/commands/check_sms.py
@@ -1,6 +1,7 @@
 from googlevoice import Voice
 from googlevoice.util import input
 from googlevoice.extractsms import extractsms
+from shoppleyuser.utils import parse_phone_number, map_phone_to_user
 from django.core.management.base import NoArgsCommand
 from datetime import datetime
 
@@ -13,41 +14,13 @@ class Command(NoArgsCommand):
 		to_be_deleted = []
 		for msg in extractsms(voice.sms.html):
 			if msg["from"] != "Me":
-				print msg
-				sender_number = parse_phone_number(msg["from"])				
-				if sender_number:
-					answerer = map_number_to_user(sender_number)
-					if not answerer: continue
-				id, body = parse_question_id(msg["text"])
-				print "id: %s" % str(id)
-				if not id: continue
-				print "body: %s" % str(body)
-				try:
-					question = Question.objects.get(pk=id)
-					# A few checks here
-					if not body:
-						print "answer is empty"
-						continue
-					if not question.answerable(): 
-						print "answer is answerable"
-						continue
-					if question.asker == answerer: 
-						print "Asker is the same as the answerer"
-						continue
-					if question.is_answered_by(answerer):
-						answer = Answer.objects.get(question=question, answerer=answerer)
-						answer.answer_text = answer.answer_text + "\n" + body
-						answer.edit_time_stamp = datetime.now()
-					else:
-						answer = Answer(answerer=answerer, question=question, time_stamp=datetime.now(),
-							answer_text=body)
-					answer.save()
+				su = map_phone_to_user(msg["from"])
+				if su and su.is_merchant():
+					details = msg["text"]
+					offer = Offer(merchant=su, description=details, time_stamp=datetime.now(),
+						starting_time=datetime.now()).save()
 					to_be_deleted.append(msg["id"])
-				except Exception, e:
-					print e
 		for message in voice.sms().messages:
-			print "start to deleting messages ..."	
-			print message.id
 			if message.id in to_be_deleted:
 				message.delete()
 		
