c48be39a1f04af887349ef7f19ecea4312425cf9
smengl <smengl@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-05-24 17:11:54 +0000

    initialize for production

---
+++ shoppley.com/shoppley/apps/offer/management/commands/initialize.py
@@ -0,0 +1,26 @@
+from django.core.management.base import NoArgsCommand
+from shoppleyuser.models import Country, Region, City, ZipCode, ShoppleyUser
+import os, csv
+from googlevoice import Voice
+
+FILE_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+class Command(NoArgsCommand):
+	def handle_noargs(self, **options):
+		f = open(FILE_ROOT+"/../../../shoppleyuser/data/US.txt", "r")
+		zip_reader = csv.reader(f, delimiter="\t")
+		for row in zip_reader:
+			country_obj, created = Country.objects.get_or_create(name="United States", code=row[0])
+			zip_code = row[1]
+			city = row[2]
+			region = row[3]
+			region_code = row[4]
+			latitude = row[9]
+			longitude = row[10]
+			region_obj, created = Region.objects.get_or_create(name=region,
+						code=region_code, country=country_obj)
+			city_obj, created = City.objects.get_or_create(name=city, region=region_obj)
+			zip_obj, created = ZipCode.objects.get_or_create(code=zip_code,
+						city=city_obj, latitude=latitude, longitude=longitude)
+
+		print "done"
