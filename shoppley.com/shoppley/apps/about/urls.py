c2bf206717b0e20141bd8e4c276a52388f9a4519
kwan <kwan@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-06-13 04:39:02 +0000

    Added about us, faq, feedback and terms

--- shoppley.com/shoppley/apps/about/urls.py
+++ shoppley.com/shoppley/apps/about/urls.py
@@ -4,6 +4,8 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, {"template": "about/about.html"}, name="about"),
     
+    url(r'^faq/$', direct_to_template, {"template": "about/faq.html"}, name="faq"),
+    url(r'^feedback/$', direct_to_template, {"template": "about/feedback.html"}, name="feedback"),
     url(r'^terms/$', direct_to_template, {"template": "about/terms.html"}, name="terms"),
     url(r'^privacy/$', direct_to_template, {"template": "about/privacy.html"}, name="privacy"),
     url(r'^dmca/$', direct_to_template, {"template": "about/dmca.html"}, name="dmca"),
