3cd04f57e12c1b9f133ec0209f3e87109880438c
kwan <kwan@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-06-19 18:27:40 +0000

    SMS_DEBUG added to dev and local_settings and STATIC_URL changed to /site_media/static/

--- shoppley.com/shoppley/settings.py
+++ shoppley.com/shoppley/settings.py
@@ -93,7 +93,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media", "static")
 
 # URL that handles the static files like app media.
 # Example: "http://media.lawrence.com"
-STATIC_URL = "/static/"
+STATIC_URL = "/site_media/static/"
 
 # Additional directories which hold static files
 STATICFILES_DIRS = [
