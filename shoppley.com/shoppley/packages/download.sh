795ab427e66fe92d24f147478bf6bf060e8b6a4f
kwan <kwan@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-06-19 19:53:07 +0000

    Installing django-mailer from git through requirements.txt

--- shoppley.com/shoppley/packages/download.sh
+++ shoppley.com/shoppley/packages/download.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 git clone https://github.com/joshuaclayton/blueprint-css.git
-git clone https://github.com/jtauber/django-mailer.git
+# django-mailer now handled by requirements.txt
+#git clone https://github.com/jtauber/django-mailer.git
 hg clone https://pygooglevoice.googlecode.com/hg/ pygooglevoice
 git clone git://github.com/jaz303/tipsy.git
