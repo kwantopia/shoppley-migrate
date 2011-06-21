269a18ce4d639c82200c10423326f7948d6d6e6c
smengl <smengl@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-06-21 15:11:35 +0000

    added templates for command msgs

--- shoppley.com/shoppley/apps/offer/utils.py
+++ shoppley.com/shoppley/apps/offer/utils.py
@@ -49,4 +49,119 @@ def pluralize(count,word):
 
 def is_chars_over_max(string):
 	return len(string)>160
+import re, htmlentitydefs
 
+##
+# Removes HTML or XML character references and entities from a text string.
+#
+# @param text The HTML (or XML) source text.
+# @return The plain text, as a Unicode string, if necessary.
+
+def unescape(text):
+	def fixup(m):
+		text = m.group(0)
+		if text[:2] == "&#":
+            	# character reference
+			try:
+				if text[:3] == "&#x":
+					return unichr(int(text[3:-1], 16))
+				else:
+					return unichr(int(text[2:-1]))
+			except ValueError:
+				pass
+		else:
+			# named entity
+			try:
+				text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+			except KeyError:
+				pass
+		return text # leave as is
+	return re.sub("&#?\w+;", fixup, text)
+
+
+from django.template import Context, Template
+
+class TxtTemplates:
+	templates = {
+		"MERCHANT": {
+			"REDEEM_WRONG_MERCHANT": "Redeem Fail! {{ code }} was initiated by a different business, and not by you.",
+			"REDEEM_SUCCESS": "{{ offer_code }} is a valid offer code! Redeemed by {{ customer }}.",
+			"REDEEM_CODE_REUSE": "Code reuse! {{ offer_code }} was redeemed by {{ customer }} at {{ time }}.",
+			"REDEEM_WRONG_CUSTOMER": "Redeem failed! {{ offer_code }} does not belong to {{ customer }}",
+			"REDEEM_INVALID_CUSTOMER_NUM": "{{ phone }} is not in our record. {{ offer_code }} offercode was not processed",
+			"REDEEM_INVALID_CODE": "{{ offer_code }} is not a valid offer code!",
+			"ZIPCODE_COMMAND_ERROR": "Command Error! To change your zipcode, please reply with #zipcode new_zipcode",
+			"ZIPCODE_CHANGE_SUCCESS": "{{ zipcode }} is your new zipcode. Your later offers will be distributed to customers in this new area.",
+			"BALANCE": "You have {{ points }} points.",
+			"OFFER_COMMAND_ERROR": "Command Error! To start an offer, please use this command: #offer offer_description",
+			"OFFER_NO_CUSTOMER" : "There were no customers that could be reached at this moment. Txt #status %s to track this offer.",
+			"OFFER_NOTENOUGH_BALANCE": "Your balance is {{ points }} points. You do not have enough to reach customers. Please try again when you have enough in your balance.",
+			"OFFER_SUCCESS": "We have received your offer at {{ time }}, {{ number }} users have been reached. Txt #status {{ code }} to track this offer: {{ offer }}",
+			"REOFFER_SUCCESS": "{{ title }} was resent to {{ resentto }} new customers.",
+			"REOFFER_NO_OFFER": "Fail to redistribute your offer! You have not started an offer yet. To start an offer, txt #offer description",
+			"REOFFER_INVALID_TRACKING" : "The tracking code {{ code }} can not be found. Please enter a correct tracking code.",
+			"REOFFER_WRONG_MERCHANT" : "Sorry offer with tracking code {{ code }} was not started by you. Please input your correct tracking code",
+			"STATUS_SUCCESS": "[%(code)s] Offer: {{ offer }} [sent to {{ sentto }}], [forwarded to {{ forwarded }}], [redeemed by {{ redeemer }} Txt #status %(code)s to track the offer",
+			"STATUS_NO_OFFER": "Fail to get status for {{ code }}! You have not started an offer yet. To start an offer, txt #offer description",
+			"STATUS_INVALID_CODE": "The tracking code {{ code }} can not be found. Please enter a correct tracking code.",
+			"STATUS_WRONG_MERCHANT" : "Sorry offer by {{ code }} was not started by you. Please txt again with your correct tracking code",
+			
+			"RESIGNUP": "You are already a Shoppley merchant.",
+			"HELP": "{{ help }}",
+			"INCORRECT_COMMAND": "{{ command }} is not available. Here are the available commands:\n {{ help }}",
+			"COMMAND_NOT_START_W_#": "{{ command }} is not a valid command. Our commands start with \"#\". Txt #help for all commands",
+			"EXPIRE_INFO": "{{ offer }} expired [sent to {{ sentto }}] [forwarded {{ forwarded }}] [redeemed {{ redeem }}]"	,
+			"HELP": "- #redeem offercode number: redeem a customer's offercode\n- #offer description: start an offer with its description.\n-#zip new_zipcode: change to a new zipcode (only support 02139 02142)- #status trackingcode: check the status of an offer you started\n- #balance: check point balance"	,
+			"SIGNUP_COMMAND_ERROR":"Fail to sign up! To sign up, txt \"#merchant email zipcode business_name\""	,
+			"SIGNUP_SUCCESS": "Sign up successfully! Please use these info to log in. Username: {{ email }}, password: {{ password }}"
+		
+		},
+		"CUSTOMER": {
+			"REDEEM_SUCCESS": "You have successfully redeemed your code at {{ merchant }}.",
+			"BALANCE" : "You have {{ points }} points.",
+
+			"OFFER_RECEIVED": "[{{ code }}] {{ title }} by {{ merchant }} (txt \"#info {{ code }}\" for address)",
+
+			"REOFFER_EXTENSION": "[{{ code }}] {{ title }}, by {{ merchant }} at {{ address }}, is extended until {{ expiration }}",
+			"REOFFER_NEWCUSTOMER_RECEIVED": "[{{ code }}] {{ title }} by {{ merchant }} (reply \"info {{ code }}\" for address)",
+
+			"INFO_NO_OFFER" : "Fail to find your offer! You have not received any offer yet.",
+			"INFO" : "Redeem [{{ offercode }}] at {{ merchant }} \"{{ description }}\" [expires: {{ expiration }}]",
+			"IWANT": "We have received your request: {{ request }}. Our search monkeys are hard at work finding the perfect deals for you.",
+			"IWANT_COMMAND_ERROR": "Command Error! To request for a deal, please reply with #iwant your_request",
+			"STOP" : "You chose to temporarily stop receiving offer messages. Please txt #start to {{ DEFAULT_SHOPPLEY }} to restart your service.",
+			"RESTOP": "You already elected to stop receiving offer messages. Please txt #start to {{ DEFAULT_SHOPPLEY }} to restart your service."
+,
+			"START": "Welcome back! You will start receiving offer messages again.",
+			"RESTART": "You are active and receiving offer messages.",
+			"ZIPCODE_COMMAND_ERROR": "Command Error! To change your zipcode, please use this command: #zipcode new_zipcode",
+			"ZIPCODE" : "{{ zipcode }} is your new zipcode. You will receive offers from this new area.",
+			"FORWARD_COMMAND_ERROR": "Fail to forward the offer! Please txt \"#forward offercode(s)\" followed by one or more friends\' numbers separated by spaces",
+			"FORWARD_WRONG_FORWARDER":"{{ code }}: Fail to forward! You are not the owner of this offercode." ,
+			"FORWARD_ALL_RECEIVED": "[{{ code }}] All phone numbers you wanted to forward the code to already received the offer.",
+			"FORWARD_SUCCESS": "Offer [{{ code }}] was forwarded to {{ numbers }}.",
+			"FORWARD_CUSTOMER_MSG": "{{ customer }} forwarded you an offer: {{ info }}. Use [{{ code }}] to redeem",
+			"FORWARD_INFO":"{{ merchant }} \"{{ description }}\" [expires: {{ expires }}]",
+			"FORWARD_NON_CUSTOMER_LOGIN": "Welcome to Shoppley! Here is your shoppley.com login info:\n - username: {{ name }}\n - password: {{ password }}" ,
+			"RESIGNUP": "You are already a Shoppley customer",
+			"INCORRECT_COMMAND": "{{ command }} command is not available. Here is a list of available commands:\n {{ help }}",
+			"COMMAND_NOT_STARTED_W_#": "{{ command }} is not a valid command. Our commands start with \"#\". Txt #help for all commands",
+			"HELP": "- #info offercode(s): list information about an offercode or offercodes separated by spaces\n- #forward offercode number(s): forward an offer to your friend(s) separated by spaces\n-#zip new_zipcode: change to a new zipcode (only support 02139 02142)\n-#stop: stop receiving messages from us\n- #start: restart receiving messages from us\n- #help: list available commands\n- #balance: check point balance",
+			"SIGNUP_COMMAND_ERROR": "Signup Error! To signup, please txt \"#signup email zipcode\"",
+			"SIGNUP_SUCCESS": "Sign up successful! Please use this info to log in. Username: {{ email }}; password: {{ password }}",
+			  
+
+		},
+		"SHARED": {
+			"INVALID_EMAIL": "\"{{ email }}\" is not a valid email address. Please provide a new and valid email.",
+			"EMAIL_TAKEN": "\"{{ email }}\" is already registered with shoppley. Please provide another email.",
+			"PHONE_TAKEN": "\"{{ phone }}\" is already registered with us. You can now use our services.",
+			"OFFERCODE_NOT_EXIST": "Offercode {{ code }} does not exist.",
+
+			"NON_USER": "Welcome to Shoppley! You are currently not one of our users. Please sign up @ {{ site }} or send us a text message to {{ shoppley_num }} with this command: \"#signup email zipcode\" to sign up as a customer or \"merchant email zipcode business_name\" to sign up as a business"
+		}
+	}
+
+	def render(tstring,c):
+		t=Template(tstring)
+		return unescape(t.render(Context(c)))
