66da05f70c12939677721b915378e76492c7fb3f
kwan <kwan@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-06-27 00:49:32 +0000

    Facebox patch added

--- shoppley.com/shoppley/patches/patch.sh
+++ shoppley.com/shoppley/patches/patch.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# patching git projects: http://tamsler.blogspot.com/2009/02/patching-with-git-diff.html
+
 X="$PWD"
+
+# dev server
+SHOPPLEY_HOME=/home/www/webuy-dev.mit.edu/shoppley
+# deployment
+#SHOPPLEY_HOME=/home/www/shoppley.com/shoppley
+# local
+#SHOPPLEY_HOME=$HOME/workspace/shoppley
+
 # TODO: need to modify PINAX_HOME to the home of your pinax environment
 PINAX_HOME=/home/virtual/shoppley-env
 #PINAX_HOME=$HOME/Documents/virtual/shoppley-env
@@ -20,4 +30,9 @@ cd $PINAX_HOME/lib/python2.7/site-packages/pinax/apps/account
 patch openid_consumer.py < account_openid_consumer.patch
 cd $X
 
-
+cd ../packages
+./download.sh
+cd $X
+cp facebox.patch $SHOPPLEY_HOME/packages/facebox
+cd $SHOPPLEY_HOME/packages/facebox
+patch -p0 < facebox.patch
