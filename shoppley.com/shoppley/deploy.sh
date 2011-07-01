03bda63d1caa30fcc56985601936eead2ca16f03
kwan <kwan@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-07-01 20:51:17 +0000

    deployment file updates

--- shoppley.com/shoppley/deploy.sh
+++ shoppley.com/shoppley/deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Script to setup the production server
+
+SHOPPLEY_HOME=/home/www/shoppley.com/shoppley
+
 source /home/virtual/shoppley-env/bin/activate
-cd /home/www/shoppley.com/shoppley
+cd $SHOPPLEY_HOME 
+
+cp patches/facebox.prod.patch $SHOPPLEY_HOME/packages/facebox
+cd $SHOPPLEY_HOME/packages/facebox
+patch -p0 < facebox.prod.patch
+
+cd $SHOPPLEY_HOME/apps/googlevoice
+cp gvoice.production gvoice
+
+cd $SHOPPLEY_HOME
+
 python manage.py collectstatic
