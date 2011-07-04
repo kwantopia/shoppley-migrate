89b3cebc469793d049d0706a5fca39cd9cb60739
kwan <kwan@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-07-04 19:15:24 +0000

    Changed merchant admin

--- shoppley.com/shoppley/apps/shoppleyuser/admin.py
+++ shoppley.com/shoppley/apps/shoppleyuser/admin.py
@@ -3,13 +3,22 @@ from shoppleyuser.models import Merchant, Customer, MerchantOfTheDay, ZipCode
 
 from django.contrib import admin
 
+class MerchantAdmin(admin.ModelAdmin):
+	list_display = ('business_name', 'admin', 'merchant_email', 'phone', 'address_1', 'zipcode')
+	list_filter = ('business_name', 'zipcode')
+	search_fields = ('business_name', 'admin', 'merchant_email', 'zipcode')
+
+	def merchant_email(self, obj):
+		return obj.user.email 
+	merchant_email.short_description = 'E-mail'
+
 admin.site.register(Country)
 admin.site.register(City)
 admin.site.register(Region)
 admin.site.register(Category)
 admin.site.register(ZipCode)
 admin.site.register(ShoppleyUser)
-admin.site.register(Merchant)
+admin.site.register(Merchant, MerchantAdmin)
 admin.site.register(MerchantOfTheDay)
 admin.site.register(Customer)
 
