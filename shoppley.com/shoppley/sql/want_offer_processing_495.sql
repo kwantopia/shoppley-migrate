9b1329be21c103ee0d3132969cecbb2d5b0d8a10
kwan <kwan@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-07-25 05:57:02 +0000

    copy time_stamp to date_created and date_modified

--- shoppley.com/shoppley/sql/want_offer_processing_495.sql
+++ shoppley.com/shoppley/sql/want_offer_processing_495.sql
@@ -2,8 +2,12 @@
 alter table shoppleyuser_iwantrequest add column processed boolean;
 
 -- to handle logical delete
-alter table offer_offer add column date_created timestamp with time zone not null;
-alter table offer_offer add column date_modified timestamp with time zone not null;
+alter table offer_offer add column date_created timestamp with time zone;
+alter table offer_offer add column date_modified timestamp with time zone;
+update offer_offer set date_created=time_stamp;
+update offer_offer set date_modified=time_stamp;
+alter table offer_offer alter column date_created SET not null;
+alter table offer_offer alter column date_modified SET not null;
 alter table offer_offer add column date_removed timestamp with time zone;
 alter table offer_offer alter column expired_time TYPE timestamp with time zone;
 
