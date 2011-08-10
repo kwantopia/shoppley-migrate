e2a97f1a4fc86a76c8c41933486718c374363855
yod <yod@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-08-10 04:10:08 +0000

    kwan should test his code... (import stuff)

--- shoppley.com/shoppley/apps/offer/models.py
+++ shoppley.com/shoppley/apps/offer/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.contrib.gis.geos import fromstr
 from django.utils.translation import ugettext as _
 from django.utils.translation import ungettext, string_concat
 
