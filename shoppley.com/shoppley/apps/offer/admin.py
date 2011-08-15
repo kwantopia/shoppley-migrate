af4638a5bd37809e0def037dd6422d4c20084158
kwan <kwan@39aed452-ab48-4dd0-a103-e9483b2b23ae>
2011-08-15 00:53:20 +0000

    Added tracking code

--- shoppley.com/shoppley/apps/offer/admin.py
+++ shoppley.com/shoppley/apps/offer/admin.py
@@ -1,4 +1,4 @@
-from offer.models import Offer, OfferCode, Transaction, BlackListWord, BlackListOffer
+from offer.models import Offer, OfferCode, Transaction, BlackListWord, BlackListOffer, TrackingCode
 from django.contrib import admin
 import logicaldelete.admin
 
@@ -14,6 +14,7 @@ class BlackListOfferAdmin(logicaldelete.admin.ModelAdmin):
 
 admin.site.register(Offer, OfferAdmin)
 admin.site.register(OfferCode)
+admin.site.register(TrackingCode)
 admin.site.register(Transaction)
 admin.site.register(BlackListWord, BlackListWordAdmin)
 admin.site.register(BlackListOffer, BlackListOfferAdmin)
